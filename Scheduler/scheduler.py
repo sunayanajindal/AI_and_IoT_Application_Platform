89a45347d8169307b73fca47feafa86c431cb348
Aman Izardar <amanizardar@gmail.com>
2022-04-02 16:28:25 +0530

    Added all dockerfiles except Bootstraper

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -2,9 +2,9 @@ from flask import Flask, request, render_template
 import threading
 from flask import flash
 from pymongo import MongoClient
-import schedule
-import time
-import datetime
+# import schedule
+# import time
+# import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,6 +13,8 @@ def home():
 
 def renderUI():
     app.use_reloader=False
+    # app.run(host='0.0.0.0',port=5010)
     app.run(port=5010)
+
 
 renderUI()
